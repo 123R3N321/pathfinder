f0ca83131b2cf614bbd6d10044817ee632f77aaf
Wea3el <wesleyz2004@gmail.com>
2024-03-29 19:03:49 -0400

    added given files from project description

---
+++ Given Files/AI Assignment vis.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import sys
+
+def plot_maze(file_path):
+    try:
+        # Load the maze from the .txt file
+        with open(file_path, "r") as file:
+            maze = np.loadtxt(file_path, delimiter=" ", usecols=range(50))
+
+        # Initialize the plot
+        fig, ax = plt.subplots(figsize=(12, 7))
+        nrows, ncols = maze.shape
+
+        # Define colors for each tile type
+        colors = {0: "white", 1: "black", 2: "blue", 5: "red", 4: "green", 3:"white"}
+
+        # Plot each tile
+        for row in range(nrows):
+            for col in range(ncols):
+                tile_color = colors.get(
+                    maze[row, col], "white"
+                )  # Default to white if unknown
+                rect = Rectangle(
+                    (col, row), 1, 1, edgecolor="black", facecolor=tile_color
+                )
+                ax.add_patch(rect)
+
+        plt.xlim(0, ncols)
+        plt.ylim(0, nrows)
+        plt.gca().invert_yaxis()  # Invert y-axis to match array layout
+        plt.axis("off")  # Turn off the axis
+        plt.show()
+
+    except Exception as e:
+        print(f"Failed to read the file: {e}")
+
+
+def main():
+    file_path = sys.argv[1]
+    plot_maze(file_path)
+
+
+
+if __name__ == "__main__":
+    main()
